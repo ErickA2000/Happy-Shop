37c0de7f75ce64da18f63f4b0bd2043f6d6bb146
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2021-10-27 14:54:52 -0500

    tercer  guardado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
